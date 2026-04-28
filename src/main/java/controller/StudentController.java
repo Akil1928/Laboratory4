8ed39f61e3c776b8c4864acd0315469e4f135b58
Akil1928 <akilwatsonch1928@gmail.com>
2026-04-27 19:32:21 -0600

    Todo listo para montar botones.

--- src/main/java/controller/StudentController.java
+++ src/main/java/controller/StudentController.java
@@ -15,7 +15,7 @@ public class StudentController
     @javafx.fxml.FXML
     private BorderPane bp;
     @javafx.fxml.FXML
-    private TableView<Student> studentTableview; //establezco el tipo para el tableview
+    private TableView<Student> studentTableview;
     @javafx.fxml.FXML
     private TableColumn<Student, String> idTableColumn;
     @javafx.fxml.FXML
@@ -25,28 +25,23 @@ public class StudentController
     @javafx.fxml.FXML
     private TableColumn<Student, String> addressTableColumn;
 
-    //defino la lista enlazada interna
     private SinglyLinkedList studentList;
-    private Alert alert; //para el manejo de alertas
+    private Alert alert;
 
     @javafx.fxml.FXML
     public void initialize() {
-        //cargamos la lista general
         this.studentList = util.Utility.getStudentList();
         alert = util.FXUtility.alert("Student List", "Display Student");
         alert.setAlertType(Alert.AlertType.ERROR);
+
         idTableColumn.setCellValueFactory(new PropertyValueFactory<>("Id"));
         nameTableColumn.setCellValueFactory(new PropertyValueFactory<>("Name"));
         ageTableColumn.setCellValueFactory(new PropertyValueFactory<>("Age"));
         addressTableColumn.setCellValueFactory(new PropertyValueFactory<>("Address"));
-        try{
-            if(studentList!=null && !studentList.isEmpty()){
-                for(int i=1; i<=studentList.size(); i++) {
-                    studentTableview.getItems().add((Student) studentList.getNode(i).data);
-                }
-            }
-            //this.studentTableView.setItems(observableList);
-        }catch(ListException ex){
+
+        try {
+            updateTableView();
+        } catch (ListException ex) {
             alert.setContentText("Student list is empty");
             alert.showAndWait();
         }
@@ -55,23 +50,15 @@ public class StudentController
     @javafx.fxml.FXML
     public void clearOnAction(ActionEvent actionEvent) {
         this.studentList.clear();
-        util.Utility.setStudentList(this.studentList); //actualizo la lista general
-        this.alert.setContentText("The list was deleted");
-        this.alert.setAlertType(Alert.AlertType.INFORMATION);
-        this.alert.showAndWait();
+        util.Utility.setStudentList(this.studentList);
+        alert.setContentText("The list was deleted");
+        alert.setAlertType(Alert.AlertType.INFORMATION);
+        alert.showAndWait();
         try {
-            updateTableView(); //actualiza el contenido del tableview
+            updateTableView();
         } catch (ListException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    @javafx.fxml.FXML
-    public void containsOnAction(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void sizeOnAction(ActionEvent actionEvent) {
     }
 
     @javafx.fxml.FXML
@@ -85,33 +72,42 @@ public class StudentController
     }
 
     @javafx.fxml.FXML
-    public void removeOnAction(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
     public void addSortedOnAction(ActionEvent actionEvent) {
+        util.FXUtility.loadPage("ucr.lab.HelloApplication", "/addSortedStudent.fxml", bp);
     }
 
     @javafx.fxml.FXML
-    public void getFirstOnAction(ActionEvent actionEvent) {
-    }
+    public void removeOnAction(ActionEvent actionEvent) {
 
-    @javafx.fxml.FXML
-    public void removeFirstOnAction(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void getLastOnAction(ActionEvent actionEvent) {
-    }
-
-    private void updateTableView() throws ListException {
-        this.studentTableview.getItems().clear(); //clear table
-        this.studentList = util.Utility.getStudentList(); //cargo la lista
-        if(studentList!=null && !studentList.isEmpty()){
-            for(int i=1; i<=studentList.size(); i++) {
-                this.studentTableview.getItems().add((Student)studentList.getNode(i).data);
+        Student selectedStudent = studentTableview.getSelectionModel().getSelectedItem();
+        if (selectedStudent != null) {
+            try {
+                studentList.remove(selectedStudent);
+            } catch (ListException e) {
+                throw new RuntimeException(e);
             }
+            util.Utility.setStudentList(studentList);
+            alert.setContentText("Student removed from the list");
+            alert.setAlertType(Alert.AlertType.INFORMATION);
+            alert.showAndWait();
+            try {
+                updateTableView();
+            } catch (ListException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            alert.setContentText("No student selected for removal");
+            alert.showAndWait();
         }
     }
 
+    private void updateTableView() throws ListException {
+        this.studentTableview.getItems().clear();
+        this.studentList = util.Utility.getStudentList();
+        if (studentList != null && !studentList.isEmpty()) {
+            for (int i = 1; i <= studentList.size(); i++) {
+                this.studentTableview.getItems().add((Student) studentList.getNode(i).data);
+            }
+        }
+    }
 }
